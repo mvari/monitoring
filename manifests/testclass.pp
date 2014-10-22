6d87fade69537738aacd27402fd405bba779f2a7
Mihai Vari <mihai.vari@gmail.com>
2014-10-22 16:57:02 +0300

    test class

---
+++ manifests/testclass.pp
@@ -0,0 +1,25 @@
+class monitoring::testclass(
+  $zabbix_server,
+  $listen_port ='80',
+  $db_password,
+  $db_type = 'mysql',
+) {
+  class { "mysql": } ->
+  mysql::grant { '${zabbix::server::db_name}':
+    mysql_username => $zabbix::server::db_user,
+    mysql_password => $db_password,
+  } ->
+  class { "apache":
+    port => $listen_port
+  } ->
+  class { 'zabbix::server':
+    db_password => $db_password
+  } ->
+  class { 'zabbix::proxy':
+    server_name => $zabbix_server,
+    db_type     => $db_type
+  } ->
+  class {'zabbix::agent':
+    zabbix_server => $zabbix_server
+  }
+}
